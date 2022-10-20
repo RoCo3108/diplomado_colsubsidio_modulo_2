7cfe8aaffa91ddbffa44312e3ca9b9a72ee8fcf8
RoCo3108 <roco272012@hotmail.com>
2022-10-20 01:15:26 -0500

    Javascript-paso-parametro

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+///console.log("javascript in amazing!")
+
+function pulsame(){
+
+    console.log("C is fun\nPython is cool\nJavascript is amazing")
+}
+
+
+function primera(parm){
+
+    if (parm===undefined){
+        console.log("argument not found")
+    }else{
+
+        console.log("Argument found")
+    }
+}
