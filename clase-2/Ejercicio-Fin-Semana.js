c0a2f80905ac97155547ebd11b743a417e186632
RoCo3108 <roco272012@hotmail.com>
2022-10-23 19:53:59 -0500

    Ejercicio-Resuelto

--- clase-2/Ejercicio-Fin-Semana.js
+++ clase-2/Ejercicio-Fin-Semana.js
@@ -1,19 +1,16 @@
-console.log("Hola Ronald Lo lograstes !Felicitaciones\n")
+//console.log("Hola Ronald Lo lograstes !Felicitaciones\n")
 
-function generateCuadrado(num, letra){
-
-    if (typeof num==="number" )  {
-        
+function generateCuadrado(num,letra){
+    if (typeof num  === "number"){
         for (let i= 0; i < num; i++) {
             console.log(letra.repeat(num))
             for (let j = 0; j <num; j++) { 
             }
-            
         }
-    }else if(letra==="x" ){
-        console.log("La letra debe ser una X Mayuscula")
     }
+     else{
+        console.log("Missing Size")
+     }
 
 }
-
 generateCuadrado(6,"X")
