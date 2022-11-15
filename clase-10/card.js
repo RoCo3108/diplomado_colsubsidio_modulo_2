7dd700cf0246ee69e1386ee776cbc3b5998500ed
RoCo3108 <roco272012@hotmail.com>
2022-11-15 18:25:38 -0500

    Modifique Card

--- clase-10/card.js
+++ clase-10/card.js
@@ -1,4 +1,98 @@
+let variasTarjetas =
+[
+  {
+      "title": "Card 1",
+      "content": "Este es el texto que debe observarse en el card-text",
+      "buton": "Pulsame"
+  },
+  {
+      "title": "Card 2",
+      "content": "Este es el texto que debe observarse en el card-text",
+      "buton": "Pulsame"
+  },
+  {
+      "title": "Card 3",
+      "content": "Este es el texto que debe observarse en el card-text",
+      "buton": "Pulsame"
+  },
+  {
+      "title": "Card 4",
+      "content": "Este es el texto que debe observarse en el card-text",
+      "buton": "Pulsame"
+  },
+  {
+      "title": "Card 5",
+      "content": "Este es el texto que debe observarse en el card-text",
+      "buton": "Pulsame"
+  },
+  {
+      "title": "Card 6",
+      "content": "Este es el texto que debe observarse en el card-text",
+      "buton": "Pulsame"
+  },
+  {
+    "title": "Card 7",
+    "content": "Este es el texto que debe observarse en el card-text",
+    "buton": "Pulsame"
+}
+  ]
 
+/*const listaPrueba =["martes", "miercoles", "jueves"];
+listaPrueba.forEach((item) => {
+  console.log(item);
+  const li = document.createElement("li");
+  li.textContent = item;
+
+  lista.appendChild(li);
+});*/
+
+
+const filasR = document.getElementById("rows");
+
+const createCard = (elemento) => {
+
+        const colum = document.createElement("div");
+       
+        const cards = document.createElement("div");
+        const headers = document.createElement("div");
+        headers.textContent = "Tarjeta de Presentacion";
+  
+        const div_body = document.createElement("div");
+  
+        const titulo = document.createElement("h5");
+        titulo.textContent = elemento.title;
+  
+        const parrafo = document.createElement("p");
+        parrafo.textContent = elemento.content;
+  
+        const boton = document.createElement("a");
+        boton.textContent = elemento.buton;
+
+        const footer = document.createElement("div");
+      
+        colum.setAttribute("class", "col-lg-4")
+        div_body.setAttribute("class", "card-body");
+        titulo.setAttribute("class", "card-title text-center");
+        headers.setAttribute("class", "card-header bg-primary text-white");
+        parrafo.setAttribute("class", "card-text ");
+        boton.setAttribute("class", "btn btn-primary");
+        cards.setAttribute("class", "card my-3")
+        footer.setAttribute("class", "card-footer")
+        filasR.appendChild(colum);
+        colum.appendChild(cards);
+        cards.appendChild(headers);
+        cards.appendChild(div_body);
+        div_body.appendChild(titulo);
+        div_body.appendChild(parrafo);
+        div_body.appendChild(boton);
+        cards.appendChild(footer);
+        div_body.appendChild(parrafo);
+        div_body.appendChild(boton);
+}
+createCard(5)
+variasTarjetas.forEach(elemento => createCard(elemento));
+
+/*
 function dimeCuantasCardsQuieresQueSea(mizin) {
   const filas = document.createElement("div");
   if (typeof mizin === "number") {
@@ -42,4 +136,5 @@ function dimeCuantasCardsQuieresQueSea(mizin) {
 
 
 
-dimeCuantasCardsQuieresQueSea(2);
+dimeCuantasCardsQuieresQueSea(1);*/
+
